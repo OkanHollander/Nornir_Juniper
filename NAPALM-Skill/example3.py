eccb0c808622945f5fb4237a5490bf69700cfb9d
Okan Hollander <o.hollander1990@gmail.com>
2024-05-28 14:17:35 +0200

    compare changes and commit

--- NAPALM-Skill/example3.py
+++ NAPALM-Skill/example3.py
@@ -5,7 +5,8 @@ juniper_device = get_network_driver('junos')
 my_device = {
     "host": "10.123.10.195",
     "auth_username": "okan",
-    "auth_password": "Welkom@1234",}
+    "auth_password": "Welkom@1234",
+}
 
 with juniper_device(
     hostname=my_device["host"],
@@ -13,3 +14,21 @@ with juniper_device(
     password=my_device["auth_password"],
 ) as device:
     device.load_replace_candidate(filename="R1.cfg")
+    # device.commit_config()
+
+    compare_result = device.compare_config()
+    if compare_result:
+        print(compare_result)
+
+        answer = input("Do you want to commit the changes? [y/n]: ")
+        while answer not in ["y", "n"]:
+            print("Please enter y or n")
+            answer = input("Do you want to commit the changes? [y/n]: ")
+
+        if answer == "y":
+            device.commit_config()
+            print("Changes are committed")
+        elif answer == "n":
+            print("Changes are not committed")
+    else:
+        print("There are no differences between the running config and the candidate config")
