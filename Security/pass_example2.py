c228e48260219bcf6f9ad364c004bcbcabb96cc4
Okan Hollander <o.hollander1990@gmail.com>
2024-06-03 16:33:30 +0200

    password example with environment variables

---
+++ Security/pass_example2.py
@@ -0,0 +1,15 @@
+import getpass
+from netmiko import ConnectHandler
+from rich import print as rprint
+import os
+
+my_pass = os.environ.get("MYPASS")
+my_user = os.environ.get("MYUSER")
+
+with ConnectHandler(device_type="juniper",
+                    host="10.123.10.195",
+                    username=my_user,
+                    password=my_pass,
+                    port=22) as ssh:
+    result = ssh.send_command("show version")
+    rprint(result)
